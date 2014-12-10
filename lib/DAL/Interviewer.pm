549f6473dd55ea6cb00066179a5a0dc67f1b0f45
Summoner <andrtur@gmail.com>
2014-12-10 14:29:16 +0300

    Interviewer

--- lib/DAL/Interviewer.pm
+++ lib/DAL/Interviewer.pm
@@ -12,111 +12,113 @@ my $dbh = lib::DB->instance();
 
 sub add{
 
-	my ($self,$interviewer) = @_;		
+    my ($self,$interviewer) = @_;		
 
-	my $sth = $dbh->prepare("INSERT INTO Interviewer
-                           (forename, 
-	    					surname,
-		    				phone_number,
-			    			email )
-                            values
-                           (?,?,?,?)");
-	$sth->execute($interviewer->forename,
-					$interviewer->surname,
-					$interviewer->phone_number,
-					$interviewer->email
-	) || die $log->error("$DBI::errstr");
+    my $sth = $dbh->prepare( "INSERT INTO Interviewer
+                              ( forename, 
+                                surname,
+                                phone_number,
+                                email )
+                                values
+                                (?,?,?,?)" );
+        $sth->execute(  $interviewer->forename,
+                        $interviewer->surname,
+                        $interviewer->phone_number,
+                        $interviewer->email ) || die $log->error("$DBI::errstr");
 	
-	$log->info("Added 1 Interviewer");
-	$sth->finish();
-
+        $log->info("Added 1 Interviewer");
+        $sth->finish();
 }
 
 sub get_by_id{
 
-	my ( $self,$id ) = @_;
+    my ( $self,$id ) = @_;
 
-	my $sth = $dbh->prepare("SELECT 
+    my $sth = $dbh->prepare("SELECT 
                             id,
-							forename,
-							surname,
-							phone_number,
-							email FROM Interviewer WHERE id=?");
-	$sth->execute( $id ) || die $log->error("$DBI::errstr");
+                            forename,
+                            surname,
+                            phone_number,
+                            email FROM Interviewer WHERE id=?");
 
-	if ($sth->rows >1 || $sth->rows == 0){
+        $sth->execute( $id ) || die $log->error("$DBI::errstr");
 
-		$log->info("We have " . $sth->rows . " Interviewers with id: $id");
-		return;
-	}
+        if ($sth->rows >1 || $sth->rows == 0){
+
+            $log->info("We have " . $sth->rows . " Interviewers with id: $id");
+}
 	
-	my @row = $sth->fetchrow_array();
-	my $interviewer = lib::Entities::Interviewer->new();
+    my @row = $sth->fetchrow_array();
+    my $interviewer = lib::Entities::Interviewer->new();
 
-   	     (  $interviewer->{id},
-            $interviewer->{forename},
-			$interviewer->{surname},
-			$interviewer->{phone_number},
-			$interviewer->{email} ) = (@row);
-	$sth->finish();
-	return $interviewer;
+      ( $interviewer->{id},
+        $interviewer->{forename},
+        $interviewer->{surname},
+        $interviewer->{phone_number},
+        $interviewer->{email} ) = (@row);
+
+    $sth->finish();
+    return $interviewer;
 }
 
 sub update_by_id{
 	
-	my ($self,$id,$interviewer) = @_;
+    my ($self,$id,$interviewer) = @_;
 		
-	my $sth = $dbh->prepare("UPDATE Interviewer
-                        	SET forename = ?,
-							surname = ?,
-							phone_number = ?,
-							email = ?
-							WHERE id= ?");
+    my $sth = $dbh->prepare( "UPDATE Interviewer
+                                SET forename = ?,
+                                surname = ?,
+                                phone_number = ?,
+                                email = ?
+                                WHERE id= ?" );
 
-$sth->execute($interviewer->forename,
-			$interviewer->surname,
-			$interviewer->phone_number,
-			$interviewer->email,
-			$id ) || die $log->error("$DBI::errstr");
+        $sth->execute(  $interviewer->forename,
+                        $interviewer->surname,
+                        $interviewer->phone_number,
+                        $interviewer->email,
+                        $id ) || die $log->error("$DBI::errstr");
 
-		$log->info("We have " . $sth->rows . " Interviewers updated with id: $id");
-		$sth->finish();
+        $log->info("We have " . $sth->rows . " Interviewers updated with id: $id");
+        $sth->finish();
 }
 
 sub delete_by_id{
 	
-	my ($self,$id) = @_;
+    my ($self,$id) = @_;
 		
-	my $sth = $dbh->prepare("DELETE FROM Interviewer
-                        WHERE id = ?");
-	$sth->execute( $id ) || die $log->error("$DBI::errstr");
-	$log->info("Deleted: " . $sth->rows . " Interviewer with id: $id");
-	$sth->finish();
+    my $sth = $dbh->prepare("DELETE FROM Interviewer WHERE id = ?");
+
+        $sth->execute( $id ) || die $log->error("$DBI::errstr");
+        $log->info("Deleted: " . $sth->rows . " Interviewer with id: $id");
+        $sth->finish();
 }
 
 sub get_list{
 
-	my $self = shift;
-	my $interviewers = [];	
+    my $self = shift;
+    my $interviewers = [];	
 	
- 	my $sth = $dbh->prepare("SELECT
-							id,
-							forename,
-							surname,
-							phone_number,
-							email FROM Interviewer");
-	$sth->execute() || die $log->error("$DBI::errstr");
+    my $sth = $dbh->prepare("SELECT
+                                id,
+                                forename,
+                                surname,
+                                phone_number,
+                                email FROM Interviewer");
+        $sth->execute() || die $log->error("$DBI::errstr");
 
-	while (my @row = $sth->fetchrow_array()) {
-			my $interviewer = lib::Entities::Interviewer->new();   	
-   		   ($interviewer->{id},
-			$interviewer->{forename},
-			$interviewer->{surname},
-			$interviewer->{phone_number},
-			$interviewer->{email} ) = (@row);
-			push @$interviewers,$interviewer;
-    }
-	$sth->finish();
-    return $interviewers;
+        while (my @row = $sth->fetchrow_array()) {
+
+            my $interviewer = lib::Entities::Interviewer->new(); 
+
+          ( $interviewer->{id},
+            $interviewer->{forename},
+            $interviewer->{surname},
+            $interviewer->{phone_number},
+            $interviewer->{email} ) = (@row);
+
+            push @$interviewers,$interviewer;
+        }
+        $sth->finish();
+        return $interviewers;
 }
 1;
